dbe0b5e2b55132068ca6775e8def6d7b56dfc48e
sridevi-inti <119276792+sridevi-inti@users.noreply.github.com>
2022-11-30 00:28:23 -0800

    back all features

--- src/test/java/testRunner/TestRunner.java
+++ src/test/java/testRunner/TestRunner.java
@@ -9,7 +9,7 @@ import io.cucumber.junit.CucumberOptions;
 @RunWith(Cucumber.class)
 
 
-@CucumberOptions(features ={"src/test/resources/features/1"}, 
+@CucumberOptions(features ={"src/test/resources/features"}, 
 glue = {"stepDefinations","Hooks"}, 
 monochrome = true, 
 plugin = {"pretty",
--- src/test/resources/features/1/1Registration_Header.feature
+++
@@ -1,6 +0,0 @@
-Feature: Header Validation
-
-  Scenario: Validating the heading of Registration form
-    Given Admin/User/Staff is on to LMS website
-    When Admin/User/Staff lands on Registration page
-    Then Admin/User/Staff see header text "Registration Form"
